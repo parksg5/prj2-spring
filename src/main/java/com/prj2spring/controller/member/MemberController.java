1500d35fc9c3a693bca922393f2d4ed8dd9cb908
parksg <park0110725@naver.com>
2024-05-22 09:54:15 +0900

    회원 가입 로직

---
+++ src/main/java/com/prj2spring/controller/member/MemberController.java
@@ -0,0 +1,22 @@
+package com.prj2spring.controller.member;
+
+import com.prj2spring.domain.member.Member;
+import com.prj2spring.service.member.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/member")
+public class MemberController {
+
+    final MemberService service;
+
+    @PostMapping("signup")
+    public void signup(@RequestBody Member member) {
+        service.add(member);
+    }
+}
