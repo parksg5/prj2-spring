573e8d2dca0e53ee366b556fdbbf89e5530100f9
parksg <park0110725@naver.com>
2024-05-22 15:11:52 +0900

    회원 목록 보기

--- src/main/java/com/prj2spring/domain/member/Member.java
+++ src/main/java/com/prj2spring/domain/member/Member.java
@@ -2,9 +2,20 @@ package com.prj2spring.domain.member;
 
 import lombok.Data;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 @Data
 public class Member {
     private String email;
     private String password;
     private String nickName;
+    private LocalDateTime inserted;
+
+    public String getSignupDateAndTime() {
+        DateTimeFormatter formatter
+                = DateTimeFormatter.ofPattern("yyyy년 MM월 dd일 HH시 mm분 ss초");
+
+        return inserted.format(formatter);
+    }
 }
