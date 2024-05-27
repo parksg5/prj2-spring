11709587ca32c7859f95aafed019853ac22fef27
parksg <park0110725@naver.com>
2024-05-27 12:20:32 +0900

    마지막 페이지네이션이 10개가 안되면 왼쪽을 채워 넣음

--- src/main/java/com/prj2spring/service/board/BoardService.java
+++ src/main/java/com/prj2spring/service/board/BoardService.java
@@ -42,10 +42,20 @@ public class BoardService {
         Integer lastPageNumber = (countAll - 1) / 10 + 1;
         Integer leftPageNumber = (page - 1) / 10 * 10 + 1;
         Integer rightPageNumber = leftPageNumber + 9;
+        rightPageNumber = Math.min(rightPageNumber, lastPageNumber);
+        leftPageNumber = rightPageNumber - 9;
+        leftPageNumber = Math.max(leftPageNumber, 1);
+        Integer prevPageNumber = leftPageNumber - 1;
+        Integer nextPageNumber = rightPageNumber + 1;
 
         // rightPageNumber는 lastPageNumber 보다 크지 않도록
         // 이전, 처음, 다음, 맨끝 버튼 만들기
-
+        if (prevPageNumber > 0) {
+            pageInfo.put("prevPageNumber", prevPageNumber);
+        }
+        if (nextPageNumber <= lastPageNumber) {
+            pageInfo.put("nextPageNumber", nextPageNumber);
+        }
         pageInfo.put("currentPageNumber", page);
         pageInfo.put("lastPageNumber", lastPageNumber);
         pageInfo.put("leftPageNumber", leftPageNumber);
