a44d2cffa40319b8fda0f35c71cd8358728a9ae2
parksg <park0110725@naver.com>
2024-05-27 15:04:25 +0900

    조회용 업데이트

--- sql/project.sql
+++ sql/project.sql
@@ -94,3 +94,16 @@ WHERE id % 2 = 0;
 UPDATE board
 SET member_id = 16
 WHERE id % 2 = 1;
+
+UPDATE board
+SET title   = 'abc def',
+    content = 'ghi jkl'
+WHERE id % 3 = 0;
+UPDATE board
+SET title   = 'mno pqr',
+    content = 'stu vwx'
+WHERE id % 3 = 1;
+UPDATE board
+SET title   = 'yz1 234',
+    content = '567 890'
+WHERE id % 3 = 2;
