f79f606a80e3d9e8b35bb962ba6227e2b43bb964
parksg <park0110725@naver.com>
2024-05-30 09:21:19 +0900

    게시물 삭제시 좋아요 먼저 삭제

--- src/main/java/com/prj2spring/mapper/board/BoardMapper.java
+++ src/main/java/com/prj2spring/mapper/board/BoardMapper.java
@@ -169,4 +169,10 @@ public interface BoardMapper {
               AND member_id = #{memberId}
             """)
     int selectLikeByBoardIdAndMemberId(Integer boardId, String memberId);
+
+    @Delete("""
+            DELETE FROM board_like
+            WHERE board_id = #{boardId}
+            """)
+    int deleteLikeByBoardId(Integer boardId);
 }
